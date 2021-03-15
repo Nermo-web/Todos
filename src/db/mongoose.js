a8e2c94ae87e0df84eb2b3a73c868e566b0f682a
Ivelin Georgiev <Ivelin.Georgiev@immedis.com>
2021-03-15 17:51:36 +0200

    MongoDB connection string. Address changed to localhost

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const connectionString = process.env.CONNECTION_STRING ||  "mongodb://172.21.32.1:27017/main_db"
+const connectionString = process.env.CONNECTION_STRING ||  "mongodb://localhost:27017/main_db"
 
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then((value) => console.log("Connected to database"))
